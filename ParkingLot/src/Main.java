3b96a722fbf8592f87ab75fc676d19b975463b6e
gaju-01 <you@example.com>
2025-03-02 21:07:12 +0530

    #10 - Refactored the code for generating the parking ticket

--- ParkingLot/src/Main.java
+++ ParkingLot/src/Main.java
@@ -15,9 +15,11 @@ public class Main {
 
         Vehicle twoWheeler = new TwoWheeler("1");
         Vehicle fourWheeler = new FourWheeler("2");
+        Vehicle fourWheeler2 = new FourWheeler("2");
 
         ParkingSpot s1 = new TwoWheelerParkingSpot(1, true, 1, ParkingStatus.AVAILABLE);
         ParkingSpot s2 = new FourWheelerParkingSpot(2, false, 2, ParkingStatus.AVAILABLE);
+        ParkingSpot s3 = new FourWheelerParkingSpot(2, false, 2, ParkingStatus.RESERVED);
 
         Entrance e1 = new Entrance(1);
         Entrance e2 = new Entrance(2);
@@ -27,6 +29,7 @@ public class Main {
 
         Ticket t1 = e1.generateTicket(1, s1, twoWheeler);
         Ticket t2 = e2.generateTicket(2, s2, fourWheeler);
+        e1.generateTicket(3, s3, fourWheeler2);
 
         t1.printTicketDetails();
         t2.printTicketDetails();
